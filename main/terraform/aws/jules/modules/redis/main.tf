789641879bc18a2c292c1a3583ed49eb18f0c8d5
Your Name <you@example.com>
2025-12-10 12:52:55 +0000

    update redis version

--- main/terraform/aws/jules/modules/redis/main.tf
+++ main/terraform/aws/jules/modules/redis/main.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "redis" {
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "redis-cart"
   engine               = "redis"
-  node_type            = "cache.t2.micro"
+  node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis7"
   engine_version       = "7.0"
