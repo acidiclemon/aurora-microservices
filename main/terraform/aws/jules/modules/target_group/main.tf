14906209aa4f0d524a81717f249eb490f0207eab
Your Name <you@example.com>
2025-12-10 14:31:12 +0000

    ts

--- main/terraform/aws/jules/modules/target_group/main.tf
+++ main/terraform/aws/jules/modules/target_group/main.tf
@@ -4,6 +4,7 @@ resource "aws_lb_target_group" "this" {
   protocol = var.protocol
   vpc_id   = var.vpc_id
   target_type = "ip"
+  deregistration_delay = 20  # Default is 300 (5 mins). Set to 0-30
 
   health_check {
     path = var.health_check_path
